36ce9268787cb20403444284ba6be31b6bcba5a8
yjyqq123 <2641617428@qq.com>
2020-10-21 15:03:02 +0800

    网盘项目初始化，引入全局样式，自定义图标库，底部导航

---
+++ components/common/nav-bar.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		<view class="fixed-top bg-white" style="background-color: #1187ff;">
+			<uni-status-bar></uni-status-bar>
+			<view style="height: 44px;" class="flex border-bottom align-center">
+				<view class="flex-1 flex"><slot name="left"></slot></view>
+				<view class="flex-1 flex justify-center"><slot></slot></view>
+				<view class="flex-1 flex justify-end"><slot name="right"></slot></view>
+			</view>
+		</view>
+		<view>
+			<uni-status-bar></uni-status-bar>
+			<view style="height: 44px;"></view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import uniStatusBar from '@/components/uni-ui/uni-status-bar/uni-status-bar.vue';
+	export default{
+		components:{
+			uniStatusBar
+		}
+	}
+</script>
+
+<style></style>
